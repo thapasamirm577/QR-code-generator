b9a0a5a6d566c7341889feb505b5cbc6921ed5b2
Samir Thapa <thapasamirm577@gmail.com>
2022-05-29 07:17:46 +0545

    modidy---

--- src/component/generate.js
+++ src/component/generate.js
@@ -1,6 +1,6 @@
-
+import makeQr from "../api";
 
 export const generator = async(data)=>{
-    const res = await fetch(`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${data}`);
+    const res = await fetch(`${makeQr}${data}`);
     return res; 
 }
